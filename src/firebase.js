8df63c0e8422e9eb24e0b7ab79a275e2c5e9e008
Negin <nuggetnegin@gmail.com>
2020-03-22 14:48:13 -0400

    setup new firebase db!

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase/app'
-import 'firebase/database'
+import firebase from 'firebase/app';
+import 'firebase/database';
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: 'AIzaSyBf6WRp2Vfq9FTfvids02HOQHtYQWY6HAc',
-  authDomain: 'wishy-washy-7388d.firebaseapp.com',
-  databaseURL: 'https://wishy-washy-7388d.firebaseio.com',
-  projectId: 'wishy-washy-7388d',
-  storageBucket: 'wishy-washy-7388d.appspot.com',
-  messagingSenderId: '810419923918',
-  appId: '1:810419923918:web:5b7446d6139d0fe39ed99f'
-}
+  apiKey: 'AIzaSyCbeKiDgDr_5DQiBtZoxgiC2QmPJLtms7E',
+  authDomain: 'bubblybubbles-8756e.firebaseapp.com',
+  databaseURL: 'https://bubblybubbles-8756e.firebaseio.com',
+  projectId: 'bubblybubbles-8756e',
+  storageBucket: 'bubblybubbles-8756e.appspot.com',
+  messagingSenderId: '239757205823',
+  appId: '1:239757205823:web:c6bf92cbfa9078e954738a'
+};
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 // this exports the CONFIGURED version of firebase
-export default firebase
+export default firebase;
