f7436dc3a00903b43f4b16a4829ee0d19e4854f6
Negin <nuggetnegin@gmail.com>
2020-03-22 15:34:50 -0400

    added resources

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,26 @@ class Footer extends Component {
           <p>
             {' '}
             I made{' '}
-            <a href="https://nuggetsnegin.github.io/wishyWashy/">
+            <a
+              href="https://nuggetsnegin.github.io/wishyWashy/"
+              target="_blank"
+            >
               WishyWashy
             </a>{' '}
             in 2019 - the first version of this project! The original intent was
             to send an anonymous wish into the wishy washy ocean. Surfing the
             'net' has been a bit hard lately, so I figured why not repurpose
-            this project into some goodness. Stay safe everyone, I miss your
-            lovely faces. - <span>Negin</span>
+            this project into some goodness. My co-worker helped compiled some
+            great mental health{' '}
+            <a
+              href="https://docs.google.com/document/d/1_uD7IAe8SqTGsbZOeTXW5lCRCnLzv0MZJ8Rx8SFurkc/edit"
+              target="_blank"
+            >
+              {' '}
+              resources
+            </a>{' '}
+            that I think are wonderful. Stay safe everyone, I miss your lovely
+            faces. - <span>Negin</span>
           </p>
         </div>
         <div>
