50f622079772d2da95a7974811c8d4727dd0e3ab
Negin <nuggetnegin@gmail.com>
2020-03-22 15:42:20 -0400

    Updates

---
+++ precache-manifest.03225bd894b62feb07303e4801fd7ce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2bb87c2ee8c2f15ccddeac41ef2cec",
+    "url": "/bubblyBubbles/index.html"
+  },
+  {
+    "revision": "3698d17cb191aa7b4a11",
+    "url": "/bubblyBubbles/static/css/main.cac2068d.chunk.css"
+  },
+  {
+    "revision": "77c72f43810e85612d78",
+    "url": "/bubblyBubbles/static/js/2.63777d2a.chunk.js"
+  },
+  {
+    "revision": "3698d17cb191aa7b4a11",
+    "url": "/bubblyBubbles/static/js/main.c1159d81.chunk.js"
+  },
+  {
+    "revision": "b3a8abdde3bdd4af1777",
+    "url": "/bubblyBubbles/static/js/runtime-main.dc3fa556.js"
+  },
+  {
+    "revision": "81f37c870899d0a43260f0da19ab069b",
+    "url": "/bubblyBubbles/static/media/scubaCat.81f37c87.png"
+  }
+]);
