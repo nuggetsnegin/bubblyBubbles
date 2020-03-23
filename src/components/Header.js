3c46be1e5d85487c112042e3bfd5e52bfc2a5ee7
tseki88 <teruhisaseki@gmail.com>
2020-03-22 20:25:24 -0400

    added bgm button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import plop from '../assets/bgm.mp3'
+const bubbleSound = new Audio(plop)
 
 class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playBgm: false
+    }
+  }
+  
+  
+  componentDidMount() {
+    bubbleSound.addEventListener("ended", function() {
+      bubbleSound.currentTime = 0;
+      bubbleSound.play()
+    })
+  }
+
+  toggleBgm = () => {
+    if (this.state.playBgm) {
+      bubbleSound.pause();
+      this.setState({
+        playBgm: false
+      })
+    } else {
+      bubbleSound.play()
+      this.setState({
+        playBgm: true
+      })
+    }
+  }
+  
   render() {
     return (
       <header className="header">
         <h1 className="knockout">Bubbly Bubbles</h1>
+        <button
+          className={`bgmButton ${this.state.playBgm ? "bgmOn" : null}`}
+          onClick={this.toggleBgm}
+          >
+          bgm {this.state.playBgm ? "off" : "on" }
+        </button>
       </header>
     );
   }
